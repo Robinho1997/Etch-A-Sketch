ae20a6a65ff32c0eb354345e90abefbd94b83a59
Robinho1997 <robin.kempen@gmx.de>
2022-04-25 22:02:51 +0200

    Erstellung der Funktion mit der  die vom  Nutzer gewünschte Felder im Container bereitgestellt werden können

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let gridParent = document.getElementById("container");
+
+// Funktion um die vom Nutzer gewünschte Größe/Anzahl der Felder anzuzeigen
+function setupGrid(size) {
+    if(size > 200) {
+        return gridParent.innerHTML = ""; // Max. Size der Felder 200
+    }
+    gridParent.innerHTML = "";
+    gridParent.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    gridParent.style.gridTemplateRows    = `repeat(${size},1fr)`;   
+}
